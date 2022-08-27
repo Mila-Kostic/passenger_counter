340fbc9951b61c59d0bf2ab3377fbff2b223e9d8
Mila Kostic <milakostic.eng@gmail.com>
2022-08-27 18:46:30 +0200

    counter

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ console.log(myAge);
 
 // document.getElementById("count").innerText = 5
 
-cmd + k + c;
+//cmd + k + c;
 let firstBatch = 5;
 let secondBatch = 7;
 let count = firstBatch + secondBatch;
@@ -21,3 +21,13 @@ let humanDogRatio = 7;
 let myDogAge = my_age * humanDogRatio;
 
 console.log(myDogAge);
+
+//counter + 1
+
+let counter = 5;
+
+counter = counter + 2;
+counter = counter + 10;
+counter = counter - 2;
+
+console.log(count); //15
