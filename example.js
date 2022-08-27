b183dd2f50e169185a1cd340911eb3370f46df38
Mila Kostic <milakostic.eng@gmail.com>
2022-08-27 18:55:16 +0200

    bP

--- example.js
+++ example.js
@@ -8,8 +8,8 @@ console.log(myAge);
 //cmd + k + c;
 let firstBatch = 5;
 let secondBatch = 7;
-let count = firstBatch + secondBatch;
-console.log(count);
+let countFinal = firstBatch + secondBatch;
+console.log(countFinal);
 
 // 1. Create two variables, myAge and humanDogRatio
 // 2. Multiply the two together and store the result in myDogAge
@@ -30,4 +30,20 @@ counter = counter + 2;
 counter = counter + 10;
 counter = counter - 2;
 
-console.log(count); //15
+console.log(counter); //15
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and than finally increase it to 70
+// Console.log the value after each step
+
+let bonusPoints = 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
