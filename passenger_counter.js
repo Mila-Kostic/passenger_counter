94cae9a427ae0669d5e0dd02a2dc6b6740e93518
Mila Kostic <milakostic.eng@gmail.com>
2022-08-27 20:33:58 +0200

    oclick event

--- passenger_counter.js
+++ passenger_counter.js
@@ -1,12 +1,20 @@
-let count = 5;
-console.log(count);
+// let count = 5;
+// console.log(count);
 
+// let saveEl = document.getElementById("save-el");
+// let countEl = document.getElementById("count-el");
 
-let saveEl = document.getElementById("save-el");
-let countEl = document.getElementById("count-el");
+// function increment() {
+//   count += 1;
+//   countEl.textContent = count;
+//   console.log(count);
+// }
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
 
 function increment() {
-  count += 1;
-  countEl.textContent = count;
-  console.log(count);
+  console.log("The button was clicked");
 }
